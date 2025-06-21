424d323df5d6f92eeafb2132e4877cbcdfdf5ee1
soomin <wzorba75@gmail.com>
2025-06-21 20:56:26 +0900

    vite.config수정

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  // base: '/prj_soom/', 
+  // base: './', 
+  base: '/prj_soom/', 
   build: {
     outDir: 'dist', // 출력 폴더 명시적 지정
     assetsInclude: ['**/*.json'],  // JSON 파일 빌드 포함
